0e4186ce2d2cc9f9fd2c4e6c32d1b7c1d0c1d90e
Jimmy Sorting <jimmy.sorting@starrepublic.com>
2020-07-21 21:27:59 +0200

    2019 solve day 6 part 1

--- 2019/Day06.fs
+++ 2019/Day06.fs
@@ -35,30 +35,47 @@ module Day06 =
         | Node(x, Empty, Empty) ->
             if x = parent 
             then Node(x, Node(value, Empty, Empty), Empty)
-            else Node (x, Empty, Empty)
+            else Node(x, Empty, Empty)
         | Node(x, left, Empty) ->
             if x = parent 
             then Node(x, left, Node(value, Empty, Empty))
-            else Node (x, addNode parent value left,  Empty)
+            else Node (x, addNode parent value left, Empty)
         | Node(x, Empty, right) ->
             if x = parent 
             then Node(x, Node(value, Empty, Empty), right)
             else Node (x, Empty, addNode parent value right)
-        | _ ->
-            failwith "Already occupied"
+        | Node(x, left, right) ->
+            if x = parent
+            then failwith "Already occupied"
+            else Node(x, addNode parent value left, addNode parent value right)
 
     let parser (s: string) =
         let [| a; b |] = s.Split(")")
         a, b      
 
+    let findRoot = function
+        | [] -> failwith "Cannot find a root with no data"
+        | list ->
+            let m, s = List.fold (fun (m', s') (a, b) -> (m' |> Map.add a (a, b)), (s' |> Set.add b)) (Map.ofList [], set []) list
+            Map.filter (fun a _ -> not (Set.contains a s)) m 
+            |> Map.toSeq 
+            |> Seq.head
+            |> snd
+            |> fun (a, b) -> Node(a, Node(b, Empty, Empty), Empty)
+
     let buildTree pairs =
         let rec aux tree = function
             | [] -> tree
-            | [(a, b)] -> addNode a b tree
-            | (a, b)::xs -> aux (tree |> addNode a b) xs
-        let a, b = List.head pairs
-        let tree = addNode a b Empty
-        aux tree (pairs |> List.tail)
+            | [(a, b)] -> 
+                printfn "%s was added to %s" b a 
+                addNode a b tree
+            | (a, b)::xs -> 
+                if not (nodeExists a tree)                
+                then aux tree (xs @ [(a, b)])
+                else 
+                    printfn "%s was added to %s" b a
+                    aux (tree |> addNode a b) xs
+        aux (findRoot pairs) pairs
 
     let countDirectAndIndirect = function
     | Empty -> 0
@@ -67,12 +84,13 @@ module Day06 =
             | Empty -> n
             | Node(_, left, right) ->
                 n + (count (n + 1) left) + (count (n + 1) right)
-        count 0 root
+        (count 0 root) / 2
 
     let part1() =
         getMany 2019 6 parser
         |> Seq.toList
         |> buildTree
+        |> fun (Node(_, tree, _)) -> tree
         |> countDirectAndIndirect
 
     let part2() = 0
