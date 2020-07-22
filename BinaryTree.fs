98359c1593d4b86a816a2f003e8cebcf0223c6ea
Jimmy Sorting <jimmy.sorting@volvocars.com>
2020-07-22 18:00:18 +0200

    Make all binar tree module functions generic

--- BinaryTree.fs
+++ BinaryTree.fs
@@ -22,18 +22,18 @@ let rec findNode value = function
             | Some x -> Some x
             | None -> findNode value right
 
-let findPath leaf = function
+let findPath value = function
     | Empty -> None
     | root ->
         let rec aux depth path = function
             | Empty -> None
-            | Node(v, left, right) ->
-                if v = leaf
+            | Node(x, left, right) ->
+                if x = value
                 then Some (path, depth)
                 else 
-                    match aux (depth + 1) (path @ [v]) left with
-                    | Some v -> Some v
-                    | None -> aux (depth + 1) (path @ [v]) right
+                    match aux (depth + 1) (path @ [x]) left with
+                    | Some x' -> Some x'
+                    | None -> aux (depth + 1) (path @ [x]) right
         aux 0 [] root
 
 let rec add parentValue value = function
@@ -55,24 +55,24 @@ let rec add parentValue value = function
         then failwith "Already occupied"
         else Node(x, add parentValue value left, add parentValue value right)
 
-let findCommonRoot leaf1 leaf2 root =        
+let findCommonRoot a b root =        
     let rec aux last = function
         | [], [] -> Some last
         | [_], [_] -> Some last
         | x1::xs1, x2::xs2 -> if x1 <> x2 then Some last else aux x1 (xs1, xs2)
         | _ -> None
 
-    let leaf1Path = match findPath leaf1 root with Some (path, _) -> path | None -> []
-    let leaf2Path = match findPath leaf2 root with Some (path, _) -> path | None -> []
+    let aPath = match findPath a root with Some (path, _) -> path | None -> []
+    let bPath = match findPath b root with Some (path, _) -> path | None -> []
 
-    findNode (match (aux "" (leaf1Path, leaf2Path)) with Some x -> x  | None -> "") root
+    findNode (match (aux Unchecked.defaultof<'a> (aPath, bPath)) with Some x -> x  | None -> Unchecked.defaultof<'a>) root
 
-let countSteps leaf1 leaf2 = function
+let countSteps a b = function
     | Empty -> 0
     | root ->
-        let leaf1Steps = match findPath leaf1 root with Some (_, steps) -> steps | None -> 1
-        let leaf2Steps = match findPath leaf2 root with Some (_, steps) -> steps | None -> 1
-        (leaf1Steps - 1) + (leaf2Steps - 1)
+        let aSteps = match findPath a root with Some (_, steps) -> steps | None -> 1
+        let bSteps = match findPath b root with Some (_, steps) -> steps | None -> 1
+        (aSteps - 1) + (bSteps - 1)
 
 let countDirectAndIndirect = function
     | Empty -> 0
