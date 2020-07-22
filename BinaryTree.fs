8c55d306e1a1627d2816302379a4e4d3e527a429
Jimmy Sorting <jimmy.sorting@volvocars.com>
2020-07-22 17:51:36 +0200

    Clean up

--- BinaryTree.fs
+++ BinaryTree.fs
@@ -4,13 +4,13 @@ type BinaryTree<'a> =
         | Node of value: 'a * left: BinaryTree<'a> * right: BinaryTree<'a>
         | Empty
 
-let rec nodeExists value = function
+let rec exists value = function
     | Empty -> false
     | Node(x, Empty, Empty) -> x = value        
     | Node(x, left, right) ->
         if x = value 
         then true
-        else nodeExists value left || nodeExists value right
+        else exists value left || exists value right
 
 let rec findNode value = function
     | Empty -> None
@@ -37,7 +37,7 @@ let findPath leaf = function
                     | None -> aux (depth + 1) (path @ [v]) right
         aux 0 [] root
 
-let rec addNode parentValue value = function
+let rec add parentValue value = function
     | Empty -> Node(value, Empty, Empty)
     | Node(x, Empty, Empty) ->
         if x = parentValue 
@@ -46,15 +46,15 @@ let rec addNode parentValue value = function
     | Node(x, left, Empty) ->
         if x = parentValue 
         then Node(x, left, Node(value, Empty, Empty))
-        else Node (x, addNode parentValue value left, Empty)
+        else Node (x, add parentValue value left, Empty)
     | Node(x, Empty, right) ->
         if x = parentValue 
         then Node(x, Node(value, Empty, Empty), right)
-        else Node (x, Empty, addNode parentValue value right)
+        else Node (x, Empty, add parentValue value right)
     | Node(x, left, right) ->
         if x = parentValue
         then failwith "Already occupied"
-        else Node(x, addNode parentValue value left, addNode parentValue value right)
+        else Node(x, add parentValue value left, add parentValue value right)
 
 let findCommonRoot leaf1 leaf2 root =        
     let rec aux last = function
