0cd4d858213404d42b8e799c500cf18f55283127
Jimmy Sorting <jimmy.sorting@volvocars.com>
2020-07-29 12:50:26 +0200

    2019 - solve day 8

---
+++ 2019/Day08.fs
@@ -0,0 +1,39 @@
+namespace Year2019
+
+module Day08 =
+    open Utilities
+
+    let imageWidth = 25
+    let imageHeight = 6
+
+    let data =
+        getSingle 2019 8 string
+        |> fun x -> x.ToCharArray()
+        |> Array.map (string >> int)
+        |> Array.toList
+        |> List.chunkBySize (imageWidth * imageHeight)
+
+    let part1 () =
+        data
+        |> List.map (fun x -> x, x |> List.filter ((=) 0) |> List.length)
+        |> List.minBy snd
+        |> fun (list, _) ->
+            (list |> List.filter ((=) 1) |> List.length)
+            * (list |> List.filter ((=) 2) |> List.length)
+
+    let part2 () =
+        data
+        |> List.collect List.indexed
+        |> List.groupBy fst
+        |> List.map
+            (snd
+             >> List.filter (snd >> (<>) 2)
+             >> List.map snd
+             >> List.head
+             >> string)        
+        |> String.concat ""         
+        |> Seq.mapi (fun i c -> if i % imageWidth = 0 then sprintf "\n%c" c else string c)
+        |> String.concat ""
+        |> fun x -> x.Replace('1', char 9608).Replace("0", " ")
+
+    let solve () = printDay 2019 8 part1 part2
