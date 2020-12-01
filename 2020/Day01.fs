aa556b8f83821fc66ab6604acf4dc7c9bd2c2ee4
Jimmy Sorting <jimmy.sorting@volvocars.com>
2020-12-01 22:21:13 +0100

    Solve day 1 2020

---
+++ 2020/Day01.fs
@@ -0,0 +1,26 @@
+namespace Year2020
+
+module Day01 =
+    open Utilities
+
+    let numbers = getMany 2020 1 (int)
+    
+    let part1() =
+        numbers
+        |> Seq.collect(fun x -> numbers |> Seq.map (fun y -> x + y, (x, y)))
+        |> Map.ofSeq
+        |> Map.tryFind 2020
+        |> function
+            | Some (x, y) -> x * y
+            | None -> failwith "Didn't find what we were looking for"
+
+    let part2() = 
+        numbers
+        |> Seq.collect(fun x -> numbers |> Seq.collect (fun y -> numbers |> Seq.map (fun z -> x + y + z, (x, y, z))))
+        |> Map.ofSeq
+        |> Map.tryFind 2020
+        |> function
+            | Some (x, y, z) -> x * y * z
+            | None -> failwith "Didn't find what we were looking for"
+
+    let solve () = printDay 2019 1 part1 part2
