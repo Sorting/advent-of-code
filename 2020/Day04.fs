e51c3cc039cbe8bbff8bfc1c53e43097a9d57efe
Jimmy Sorting <jimmy.sorting@volvocars.com>
2020-12-05 12:31:29 +0100

    refactor

--- 2020/Day04.fs
+++ 2020/Day04.fs
@@ -5,22 +5,6 @@ module Day04 =
     open System.Text.RegularExpressions
     open System
 
-    type HeightUnit =
-        | Cm
-        | In
-
-    type FieldType =
-        | Year of int
-        | Height of int * HeightUnit
-        | HairColor of string
-        | EyeColor of string
-        | PassportId of string
-        | Invalid
-
-    type FieldValidationResult =
-        | Ok
-        | Failed
-
     let parser input =
         Regex.Split(input, "\n\n")
         |> Array.filter (String.IsNullOrWhiteSpace >> not)
@@ -34,53 +18,33 @@ module Day04 =
             |> Map.ofArray)
 
     let passports = getSingle 2020 4 parser
-    let requiredFields = set ["byr"; "iyr"; "eyr"; "hgt"; "hcl"; "ecl"; "pid" ]
-    let hasRequiredFields passport = requiredFields |> Set.forall (fun rf -> Map.containsKey rf passport)
-
-    let tryParseYear (value: string) =
-        match Int32.TryParse value with
-        | true, x -> Year(x)
-        | _ -> Invalid
-
+    let requiredFields = set ["byr"; "iyr"; "eyr"; "hgt"; "hcl"; "ecl"; "pid"]
     let validEyeColors = set ["amb"; "blu"; "brn"; "gry"; "grn"; "hzl"; "oth"]
 
+    let hasValidRange (value: string) min max = 
+        match Int32.TryParse value with 
+        | true, x -> x >= min && x <= max
+        | _ -> false
+    
+    let hasRequiredFields passport = Set.forall (fun rf -> Map.containsKey rf passport) requiredFields
+
     let hasValidFieldValue = function
-        | ("byr", value) -> 
-            match tryParseYear value with
-            | Year year when year >= 1920 && year <= 2002 -> Ok
-            | _ -> Failed
-        | ("iyr", value) -> 
-            match tryParseYear value with
-            | Year year when year >= 2010 && year <= 2020 -> Ok
-            | _ -> Failed
-        | ("eyr", value) -> 
-            match tryParseYear value with
-            | Year year when year >= 2020 && year <= 2030 -> Ok
-            | _ -> Failed
+        | ("byr", value) -> hasValidRange value 1920 2002
+        | ("iyr", value) -> hasValidRange value 2010 2020
+        | ("eyr", value) -> hasValidRange value 2020 2030
         | ("hgt", value) ->
             match value with
             | Regex "^(\\d+)(cm|in)$" [height; unit] ->
-                let heightField = 
                     match unit with
-                    | "cm" -> Height(int height, Cm)
-                    | "in" -> Height(int height, In)
-                    | _ -> Invalid
-                match heightField with
-                | Height(height, Cm) when height >= 150 && height <= 193 -> Ok
-                | Height(height, In) when height >= 59 && height <= 76 -> Ok
-                | _ -> Failed
-            | _ -> Failed
-        | ("hcl", value) -> 
-            match value with
-            | Regex "^#([0-9a-f]{6})$" [_] -> Ok
-            | _ -> Failed 
-        | ("ecl", value) -> if Set.contains value validEyeColors then Ok else Failed
-        | ("pid", value) ->
-            match value with
-            | Regex "^([0-9]{9})$" [_] -> Ok
-            | _ -> Failed
-        | ("cid", _) -> Ok
-        | _ -> Failed
+                    | "cm" -> hasValidRange height 150 193
+                    | "in" -> hasValidRange height 59 76
+                    | _ -> false
+            | _ -> false
+        | ("hcl", value) -> match value with Regex "^#([0-9a-f]{6})$" [_] -> true | _ -> false
+        | ("ecl", value) -> Set.contains value validEyeColors
+        | ("pid", value) -> match value with Regex "^([0-9]{9})$" [_] -> true | _ -> false
+        | ("cid", _) -> true
+        | _ -> false
 
     let part1() = 
         passports
@@ -89,7 +53,7 @@ module Day04 =
 
     let part2() =
         passports
-        |> Array.filter (fun fields -> hasRequiredFields fields && (Map.forall (fun key value -> hasValidFieldValue (key, value) = Ok) fields))
+        |> Array.filter (hasRequiredFields <&&> (Map.forall (fun key value -> hasValidFieldValue (key, value))))
         |> Array.length
 
     let solve () = printDay 2020 4 part1 part2
