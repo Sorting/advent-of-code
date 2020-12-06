2e70d9ecdc4ae8e5f434f2961e4d243e9d238fec
Jimmy Sorting <jimmy.sorting@volvocars.com>
2020-12-06 13:20:55 +0100

    Solve day 6

---
+++ 2020/Day06.fs
@@ -0,0 +1,33 @@
+namespace Year2020
+
+module Day06 =
+    open System
+    open Utilities
+
+    let parser (input: string) =
+        input.Split("\n\n", StringSplitOptions.RemoveEmptyEntries)
+        |> Array.map (fun groups ->
+            groups.Split([| '\n'; ' ' |], StringSplitOptions.RemoveEmptyEntries)
+            |> Array.map (fun answers -> answers.ToCharArray() |> List.ofArray)            
+            |> List.ofArray)
+        |> List.ofArray
+
+    let groupAnswers = getSingle 2020 6 parser
+    
+    let part1() = 
+        groupAnswers 
+        |> List.map (List.collect id >> Set.ofList)
+        |> List.fold (fun acc x -> acc + Set.count x) 0
+    
+    let part2() =
+        groupAnswers 
+        |> List.map (fun members -> 
+            members 
+            |> List.collect id 
+            |> List.countBy id 
+            |> Map.ofList 
+            |> Map.filter (fun _ count -> count = List.length members) 
+            |> Map.count)
+        |> List.reduce (+)
+
+    let solve () = printDay 2020 6 part1 part2
--- Program.fs
+++ Program.fs
@@ -17,7 +17,7 @@ let main argv =
               ((2019, 7), Year2019.Day07.solve); ((2019, 8), Year2019.Day08.solve); ((2019, 9), Year2019.Day09.solve)
               ((2019, 10), Year2019.Day10.solve)
               ((2020, 1), Year2020.Day01.solve); ((2020, 2), Year2020.Day02.solve); ((2020, 3), Year2020.Day03.solve)
-              ((2020, 4), Year2020.Day04.solve); ((2020, 5), Year2020.Day05.solve);
+              ((2020, 4), Year2020.Day04.solve); ((2020, 5), Year2020.Day05.solve); ((2020, 6), Year2020.Day06.solve);
             ]
 
     match argv |> List.ofArray with
