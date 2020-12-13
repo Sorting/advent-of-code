9b191226e937df6ca8a79b77381c2411410130db
Jimmy Sorting <jimmy.sorting@volvocars.com>
2020-12-13 20:13:19 +0100

    Solve day 2

--- 2020/Day10.fs
+++ 2020/Day10.fs
@@ -4,33 +4,33 @@ open System.Collections.Generic
 module Day10 =
     open Utilities
 
-    let mutable adapters = getMany 2020 10 (bigint.Parse) |> Seq.sort |> List.ofSeq |> fun xs -> bigint 0 :: xs @ [List.max xs + bigint 3]
+    let mutable adapters = getMany 2020 10 (int64) |> Seq.sort |> List.ofSeq |> fun xs -> 0L :: xs @ [List.max xs + 3L]
 
-    let rec findDiffs counter (last: bigint) = function
+    let rec findDiffs counter last = function
         | [] -> (fst counter, snd counter), fst counter * snd counter
         | x :: xs ->
             let (ones, threes) = counter
             match int (x - last) with
-            | 1 -> findDiffs (ones + 1I, threes) x xs
-            | 3 -> findDiffs (ones, threes + 1I) x xs
+            | 1 -> findDiffs (ones + 1L, threes) x xs
+            | 3 -> findDiffs (ones, threes + 1L) x xs
             | _ -> findDiffs counter x xs
 
-    let dict = Dictionary<int, bigint>()
+    let dict = Dictionary<int, int64>()
 
     let rec countValidArrangements count pos =
-        if pos = adapters.Length-1 then 1I
+        if pos = adapters.Length-1 then 1L
         else match dict.TryGetValue pos with
                 | true, value  -> value
                 | _ ->
                     let count' = 
                         [ for i in pos + 1 .. (match pos + 3 with x when x < adapters.Length -> x | _ -> adapters.Length - 1) ->
-                            if adapters.[i] - adapters.[pos] <= 3I 
+                            if adapters.[i] - adapters.[pos] <= 3L
                             then count + countValidArrangements count i
-                            else 0I ] |> List.sum
+                            else 0L ] |> List.sum
                     dict.[pos] <- count'
                     count'
 
-    let part1() = findDiffs (bigint 0, bigint 0) 0I adapters
-    let part2() = countValidArrangements 0I 0
+    let part1() = findDiffs (0L, 0L) 0L adapters
+    let part2() = countValidArrangements 0L 0
 
     let solve () = printDay 2020 10 part1 part2
