7ee1fce471838547d68b2876fade95dbe799c993
Jimmy Sorting <jimmy.sorting@volvocars.com>
2021-08-12 19:37:58 +0200

    Solve day 14 part 2

--- 2020/Day14.fs
+++ 2020/Day14.fs
@@ -2,6 +2,7 @@ namespace Year2020
 
 module Day14 =
     open Utilities
+    open AdventOfCode
     open System
 
     type Instruction = 
@@ -22,23 +23,64 @@ module Day14 =
     let instructions = getMany 2020 14 parser |> Seq.toList
 
     let applyMask (bitMask: string) (value: int64) =        
-        let b = Convert.ToString(value, 2).PadLeft(36, '0').ToCharArray() |> Array.rev
-        Seq.rev bitMask 
+        let b = Convert.ToString(value, 2).PadLeft(36, '0').ToCharArray() 
+        bitMask 
         |> Seq.iteri (fun i x -> if x <> 'X' then b.[i] <- x)
-        Convert.ToInt64(Array.rev b |> Array.map string |> String.concat "", 2)
+        Convert.ToInt64(Array.map string b |> String.concat "", 2)
+
+    let getAll arr = 
+        let n = Array.length arr
+        [ for i in 0..n -> 
+            List.concat [ String('0', n - i).ToCharArray() |> List.ofArray
+                          String('1', i).ToCharArray() |> List.ofArray ]
+                        |> List.permutations
+                        |> Seq.distinct ]
+        |> Seq.collect id
+        |> List.ofSeq
+        |> List.map (fun x -> x |> List.mapi (fun i x -> arr.[i], x))
+    
+    let applyMaskV2 (bitMask: string) (value: int64) =
+        let b = Convert.ToString(value, 2).PadLeft(36, '0').ToCharArray()
+        let bits = 
+             bitMask 
+             |> Seq.fold (fun (idx, acc) x ->
+                match x with
+                | '0' ->  idx+1, acc 
+                | '1' -> 
+                    b.[idx] <- '1'
+                    idx+1, acc 
+                | 'X' ->
+                    idx+1, idx :: acc
+                | _ -> failwithf "%c is not an expected character in the bit mask" x
+               ) (0, [])
+            |> snd
+            |> List.toArray
+            |> getAll 
+            |> List.map (fun idxs -> 
+                let temp = Array.copy b
+                idxs |> List.iter (fun (idx, value) -> temp.[idx] <- value)
+                temp)
+        if List.isEmpty bits then [ Convert.ToInt64(String b, 2) ]
+        else bits |> List.map (fun bit -> Convert.ToInt64(String bit, 2))
 
     let rec executeInstructions computer = function
         | [] -> computer
         | x :: xs ->
             match x with
             | SetBitMask bitMask         -> executeInstructions { computer with BitMask = bitMask } xs
-            | SetMemory (address, value) -> executeInstructions { computer with Memory = (Map.add address (applyMask computer.BitMask value) computer.Memory) } xs
+            | SetMemory (address, value) -> executeInstructions { computer with Memory = computer.Memory |> Map.add address (applyMask computer.BitMask value) } xs
 
-    let part1() = 
-        let computer = executeInstructions { BitMask = ""; Memory = Map.empty } instructions
-        Map.toList computer.Memory |> List.sumBy snd
+    let rec executeInstructionsV2 computer = function
+        | [] -> computer
+        | x :: xs ->
+            match x with
+            | SetBitMask bitMask         -> executeInstructionsV2 { computer with BitMask = bitMask } xs
+            | SetMemory (address, value) -> executeInstructionsV2 { computer with Memory = applyMaskV2 computer.BitMask address |> List.fold (fun memory address -> Map.add address value memory) computer.Memory } xs
 
-    let part2() = 0
+    let solver f = Map.toList (f { BitMask = ""; Memory = Map.empty } instructions).Memory |> List.sumBy snd
+
+    let part1() = solver executeInstructions
+    let part2() = solver executeInstructionsV2
 
     let solve () = printDay 2020 14 part1 part2
     
--- List.fs
+++ List.fs
@@ -4,7 +4,6 @@ module List =
 
     // From: http://stackoverflow.com/questions/286427/calculating-permutations-in-f
     // Much faster than anything else I've tested
-
     let rec permutations =
         function
         | [] -> seq [ List.empty ]
@@ -14,5 +13,5 @@ module List =
         function
         | [] -> [ [ x ] ]
         | (y :: ys) as xs ->
-            (x :: xs)
-            :: (List.map (fun x -> y :: x) (insertions x ys))
+            (x :: xs) :: (List.map (fun x -> y :: x) (insertions x ys))
+    
